00505c5799094d5b346d6b3a5e06a4093af75676
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-25 17:42:00 -0400

    Move matrices into a module

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,3 @@
 //! Data storage schemes.
 
-extern crate matrix;
-
-pub use matrix::BandMatrix;
-pub use matrix::{CompressedMatrix, CompressedFormat};
-pub use matrix::DiagonalMatrix;
-pub use matrix::{PackedMatrix, PackedFormat};
+pub mod matrix;
---
+++ src/matrix.rs
@@ -0,0 +1,8 @@
+//! Matrix storage schemes.
+
+extern crate matrix;
+
+pub use self::matrix::BandMatrix;
+pub use self::matrix::{CompressedMatrix, CompressedFormat};
+pub use self::matrix::DiagonalMatrix;
+pub use self::matrix::{PackedMatrix, PackedFormat};
